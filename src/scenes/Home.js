cbaf1ad2af6e290f9d56032ac53bfcbb66dfadbf
Randy <randykeller1193@gmail.com>
2021-11-07 14:19:26 -0500

    newest design

--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -6,7 +6,9 @@ import Office from "../components/Office";
 import Blm from "../components/Blm";
 import Mural4 from "../components/Mural4";
 import Mural5 from "../components/Mural5";
-import BirdMural from "../components/BirdMural";
+import Mural6 from "../components/Mural6";
+import Mural8 from "../components/Mural8";
+import Mural9 from "../components/Mural9";
 
 function Loader() {
   const { progress } = useProgress();
@@ -29,7 +31,7 @@ function Home() {
         rotation={[0, 1.7, 0]}
       />
       <Mural4
-        position={[-0.2, 7, -52.5]}
+        position={[-6.2, 7, -52.5]}
         scale={[4.9, 3.5, 3]}
         rotation={[0, -3.17, 0]}
       />
@@ -38,12 +40,21 @@ function Home() {
         scale={[2.4, 2.6, 1]}
         rotation={[0, -3.15, 0]}
       />
-      <BirdMural
-        position={[3.5, 11, 25]}
-        scale={[3.8, 4, 2.8]}
-        rotation={[0, 0.369, 0]}
+      <Mural6
+        position={[-18.9, 0.1, 24]}
+        scale={[2, 1.92, 2]}
+        rotation={[0, 1.6, 0]}
       />
-
+      <Mural8
+        position={[8.37, 3, 17.75]}
+        scale={[3.15, 2.2, 2]}
+        rotation={[0, -1.17, -0.02]}
+      />
+      <Mural9
+        position={[14.89, 0.2, -43.65]}
+        scale={[0.55, 0.55, 0.5]}
+        rotation={[0, -0.1, 0]}
+      />
       <Avatar5 />
       <Office position={[-20, 0, 30]} scale={[2.5, 2.5, 2.5]} />
     </Suspense>
