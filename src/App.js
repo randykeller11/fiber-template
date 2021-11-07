eff378d62754455004ec8a40c7f5c5ab47b887c4
Randy <randykeller1193@gmail.com>
2021-11-07 08:28:08 -0500

    current design

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
 import "./App.css";
 import React, { Suspense } from "react";
-import { Sky } from "@react-three/drei";
+import { Sky, useProgress, Html } from "@react-three/drei";
 
 import { Canvas } from "@react-three/fiber";
 import Avatar5 from "./components/Avatar5";
 import Office from "./components/Office";
-import NewMural from "./components/NewMural";
-import Mural3 from "./components/Mural3";
-
+import Mural4 from "./components/Mural4";
 import Blm from "./components/Blm";
+
+function Loader() {
+  const { progress } = useProgress();
+  return <Html center>{progress} % loaded</Html>;
+}
+
 function App() {
   return (
     <>
       <Suspense fallback={null}>
         <Canvas camera={{ position: [0, 4, -6] }}>
-          <ambientLight intensity={0.9} />
-          <Sky
-            sunPosition={[0, 1, 0]}
-            inclination={0}
-            azimuth={0.25}
-            distance={450000}
-          />
-          <Blm
-            position={[-0.2, -0.2, -49]}
-            scale={[5, 2.5, 1]}
-            rotation={[0, 0.14, 0]}
-          />
+          <Suspense fallback={<Loader />}>
+            <ambientLight intensity={0.9} />
+            <Sky
+              sunPosition={[0, 1, 0]}
+              inclination={0}
+              azimuth={0.25}
+              distance={450000}
+            />
+            <Blm
+              position={[-19.7, -0.2, -44]}
+              scale={[2.4, 2.55, 0.3]}
+              rotation={[0, 1.7, 0]}
+            />
+            <Mural4
+              position={[-0.2, 7, -52.5]}
+              scale={[4.9, 3.5, 3]}
+              rotation={[0, -3.17, 0]}
+            />
 
-          <NewMural
-            position={[2, 2.7, 19.49]}
-            scale={[2.3, 2.2, 2.6]}
-            rotation={[0, -2.85, 0]}
-          />
-
-          <Avatar5 />
-          <Office position={[-20, 0, 30]} scale={[2.5, 2.5, 2.5]} />
+            <Avatar5 />
+            <Office position={[-20, 0, 30]} scale={[2.5, 2.5, 2.5]} />
+          </Suspense>
         </Canvas>
       </Suspense>
     </>
